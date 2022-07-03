674378eff1285310f6e71029d99d6464f9f98c55
rom <47617159+leedokchidok19@users.noreply.github.com>
2022-07-03 20:26:16 +0900

    add Reset Button

--- src/components/habits.jsx
+++ src/components/habits.jsx
@@ -49,6 +49,9 @@ class Habits extends Component {
             />
           ))}
         </ul>
+        <button className="habits-reset" onClick={this.props.onReset}>
+          Reset All
+        </button>
       </div>
     );
   }
